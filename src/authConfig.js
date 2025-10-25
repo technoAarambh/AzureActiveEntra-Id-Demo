bbe23c4ccca1102671e57cc321aa650ff8d6765c
amey <agnihotriamey19@gmail.com>
2025-10-25 18:08:54 +0530

    New code pushed

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
   auth: {
-    clientId: "6199a1fc-16e0-4985-9a17-d1e6043ae18a", // From Azure App Registration
+    clientId: "YOUR_CLIENT_ID", // From Azure App Registration
     authority: "https://login.microsoftonline.com/YOUR_TENANT_ID", // Tenant ID
     redirectUri: "http://localhost:3000",
   },
